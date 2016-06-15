a7b08036c6cb45ab06cb42e4f2c7c32bdd2d9bc7
zhangzongshan <zhangzongshan@126.com>
2016-06-15 11:08:02 +0800

    init

--- _assets/js/plugins.js
+++ _assets/js/plugins.js
@@ -24,66 +24,53 @@
 }());
 
 (function () {
+    function getScriptRoot(scriptName) {
+        var scripts = document.getElementsByTagName("script");
+        for (var n = 0; n < scripts.length; n++) {
+            var script = scripts[n];
+            if (script.src.match(eval("/" + scriptName + "(\.min|)\.js/"))) {
+                return (script.src).split("/").slice(0, -1).join("/") + "/";
+            }
+        }
+        return "";
+    }
+
+    var sysSpinkit = null;
+    var rootpath = getScriptRoot('plugins').replace('_assets/js/', '');
     Modernizr.load([{
         load: {
-            'jquery': '_assets/js/public/jquery/jquery-1.12.3.min.js'//jquery
-            , 'lodash': '_assets/js/public/underscore/underscore.js'//基础库
-            , 'html5media': '_assets/js/public/html5media/html5media-1.1.8.min.js'//多媒体
-            , 'moment': '_assets/js/public/moment/moment-local-2.1.3.min.js'//日期
-            , 'validator': '_assets/js/public/validator/validator-5.2.0.min.js'//验证
-            , 'common': '_assets/js/module/common/common.js'//加载动画
-            , 'spink': '_assets/js/module/spinkit/spinkit.js'//加载动画
-            , 'noty': '_assets/js/module/notykit/notykit.js'//对话框
-            , 'tabskit': '_assets/js/module/tabskit/tabskit.js'//对话框
+            'jquery': rootpath + '_assets/js/public/jquery/jquery-1.12.3.min.js'//jquery
+            , 'spink': rootpath + '_assets/js/module/spinkit/spinkit.js'//加载动画
         },
         callback: function (url, result, key) {
             //alert(url);
         },
         complete: function () {
-            moment.locale('zh-cn');
-
-            var tabskitobj=TabsKit.Create({
-                obj:$("#2")
-                ,id:'asdsdfs001'
-                ,tabs:[
-                    {
-                        id:'011'
-                        ,html:'测试一'
-                        ,normal:'tabsClass'
-                        ,active:'activeTabs'
-                        ,fn:function () {
-                            //alert('01');
-                        }
-                        ,content:'sadfsdafsd00111'
-                    },{
-                        id:'012'
-                        ,html:'测试二'
-                        ,normal:'tabsClass'
-                        ,active:'activeTabs'
-                        ,fn:function () {
-                            //alert('02');
-                        }
-                        ,content:'sadfsdafsd00222'
-                    },{
-                        id:'013'
-                        ,html:'测试三'
-                        ,normal:'tabsClass'
-                        ,active:'activeTabs'
-                        ,fn:function () {
-                            //alert('03');
-                        }
-                        ,content:'sadfsdafsd00333'
-                    }
-                ]
-                ,showtabs:'012'
-                ,direct:''
-                ,autowidth:true
-                ,container:$("#3")
-                ,storge:true
+            sysSpinkit = SpinKit.Create({
+                color: '#fff'
             });
-
-            var aa='';
-
+        }
+    }, {
+        load: {
+            'lodash': rootpath + '_assets/js/public/underscore/underscore.js'//基础库
+            , 'html5media': rootpath + '_assets/js/public/html5media/html5media-1.1.8.min.js'//多媒体
+            , 'moment': rootpath + '_assets/js/public/moment/moment-local-2.1.3.min.js'//日期
+            , 'validator': rootpath + '_assets/js/public/validator/validator-5.2.0.min.js'//验证
+            , 'common': rootpath + '_assets/js/module/common/common.js'//加载通用函数
+            , 'noty': rootpath + '_assets/js/module/notykit/notykit.js'//对话框
+            , 'dataload': rootpath + '_assets/js/module/dataload/dataload.js'//数据请求
+            , 'tabskit': rootpath + '_assets/js/module/tabskit/tabskit.js'//TAB
+            , 'seajs': rootpath + '_assets/js/frame/seajs/sea.js'//
+            , 'seajs-css': rootpath + '_assets/js/frame/seajs/seajs-css.js'//
+            , 'seajs-preload': rootpath + '_assets/js/frame/seajs/seajs-preload.js'//数据请求
+        },
+        callback: function (url, result, key) {
+            //alert(url);
+        },
+        complete: function () {
+            pageInit();
+            sysSpinkit.remove();
+            sysSpinkit = null;
         }
     }]);
 }());
